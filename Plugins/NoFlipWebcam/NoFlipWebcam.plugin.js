df1c69c46ecb6e176e4cfc6887e1b82efa619fc8
dededed6 <dededed338@gmail.com>
2026-03-18 22:04:11 +0900

    Update NoFlipWebcam

--- Plugins/NoFlipWebcam/NoFlipWebcam.plugin.js
+++ Plugins/NoFlipWebcam/NoFlipWebcam.plugin.js
@@ -2,13 +2,12 @@
  * @name NoFlipWebcam
  * @author dededed6
  * @version 1.0.1
- * @description Disables the mirror effect on webcam video
+ * @description Disables the mirror effect on your webcam
  * @website https://github.com/dededed6/BetterDiscordPlugins
  * @source https://raw.githubusercontent.com/dededed6/BetterDiscordPlugins/master/NoFlipWebcam/NoFlipWebcam.plugin.js
  */
 
-module.exports = class NoFlipWebcam
-{
+module.exports = class NoFlipWebcam {
     constructor() {}
     start() { BdApi.DOM.addStyle("NoFlipWebcam", `[class*="mirror__"], [class^="camera__"] { transform: scaleX(1) !important; }`); }
     stop() { BdApi.DOM.removeStyle("NoFlipWebcam"); }
