e5787cbe1ab66b42451bb8b599ee212e91e40647
dededed6 <clasp-blur.5t@icloud.com>
2026-03-25 16:09:16 +0900

    Add AlwaysTrustDoamins

---
+++ Plugins/AlwaysTrustDomains/AlwaysTrustDomains.plugin.js
@@ -0,0 +1,22 @@
+/**
+ * @name AlwaysTrustDomains
+ * @author dededed1024
+ * @version 1.0.0
+ * @description Skip link warning dialog
+ * @website https://github.com/dededed1024/BetterDiscordPlugins
+ * @source https://raw.githubusercontent.com/dededed1024/BetterDiscordPlugins/master/AlwaysTrustDomains/AlwaysTrustDomains.plugin.js
+ */
+
+module.exports = class AlwaysTrustDomains {
+    
+    linkClickHandler = (event) => {
+        const link = event.target.closest("a[href^='http']");
+        if (!link) return;
+
+        event.stopPropagation();
+        window.open(link.href, "_blank");
+    };
+
+    start() { document.body.addEventListener("click", this.linkClickHandler, true); }
+    stop() { document.body.removeEventListener("click", this.linkClickHandler, true); }
+};
